4fdda78cb90ebfcfa827839634dcdf5b5842ac2d
Victor Jorge Carvalho Chaves <victorchaves03@gmail.com>
2022-06-05 18:39:09 -0300

    task 7 done

--- Algorithms and Data Structure II/Tasks/task 7/main.c
+++ Algorithms and Data Structure II/Tasks/task 7/main.c
@@ -9,12 +9,12 @@ int get_index(char c)
     if (c <= 32)
         return 0;
     else
-        return c - 96
+        return c - 96;
 }
 
-void counting_sort(string *a, string *b, int n, string k, int d)
+string *counting_sort(string *a, string *b, int n, int k, int d)
 {
-    int *c = (int *)malloc(sizeof(int) * strlen(k));
+    int *c = (int *)malloc(sizeof(int) * k);
 
     int i;
 
@@ -24,32 +24,26 @@ void counting_sort(string *a, string *b, int n, string k, int d)
     for (i = 0; i < n; i++)
         c[get_index(a[i][d])]++;
 
-    for (i = 1; i < k; i++)
+    for (i = 0; i < k; i++)
+    {
         c[i] += c[i - 1];
+        printf("%d ", c[i]);
+    }
+
+    printf("\n");
 
     for (i = n - 1; i >= 0; i--)
-        b[--c[a[i]]] = a[i];
-}
+        b[--c[get_index(a[i][d])]] = a[i];
 
-string make_alphabet()
-{
-    string alphabet = (string)malloc(sizeof(char) * 27);
-
-    int i;
-
-    alphabet[0] = ' ';
-
-    for (i = 1; i < 27; i++)
-        alphabet[i] = (char)(96 + i);
-
-    return alphabet;
+    return b;
 }
 
 int main(int argc, char const *argv[])
 {
     string *strings;
+    string *output;
 
-    int length, i, j, word_length = 0, max_word = 0;
+    int start, number_words, length, i, j, word_length = 0, max_word = 0;
 
     scanf("%d", &length);
 
@@ -60,7 +54,13 @@ int main(int argc, char const *argv[])
         strings[i] = (string)malloc(sizeof(char) * 20);
 
         scanf("%s", strings[i]);
+    }
 
+    scanf("%d", &start);
+    scanf("%d", &number_words);
+
+    for (i = 0; i < length; i++)
+    {
         word_length = strlen(strings[i]);
 
         if (max_word < word_length)
@@ -77,6 +77,19 @@ int main(int argc, char const *argv[])
     }
 
     printf("%d\n", max_word);
+
+    for (i = max_word - 1; i >= 0; i--)
+    {
+        output = (string *)malloc(sizeof(string) * length);
+        strings = counting_sort(strings, output, length, 27, i);
+    }
+
+    for (i = start - 1; i < start + number_words - 1; i++)
+    {
+        printf("%s\n", strings[i]);
+        free(strings[i]);
+        free(output[i]);
+    }
 
     return 0;
 }
