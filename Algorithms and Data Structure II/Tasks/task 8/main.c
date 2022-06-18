cd194aef215e86aaf754622ee8a26795ebadebe2
Victor Jorge Carvalho Chaves <victorchaves03@gmail.com>
2022-06-18 12:34:36 -0300

    update

--- Algorithms and Data Structure II/Tasks/task 8/main.c
+++ Algorithms and Data Structure II/Tasks/task 8/main.c
@@ -12,6 +12,8 @@ struct tree
 
 typedef struct tree tree;
 
+tree *remove_tree(tree *node, int value, int *found);
+
 tree *create_tree(int value)
 {
     tree *node = (tree *)malloc(sizeof(tree));
@@ -24,93 +26,343 @@ tree *create_tree(int value)
     return node;
 }
 
-int insert_tree(tree *node, int value)
+tree *balance_tree(tree *father)
 {
-    int result;
+    tree *child, *grand_child;
 
-    if (node->value < value)
+    if (father->balance == 2)
     {
-        if (node->left_node == NULL)
-        {
-            node->left_node = create_tree(value);
-            node->balance++;
+        child = father->left_node;
 
-            if (node->right_node == NULL)
-                return 1;
-            else
-                return 0;
+        if (child->balance >= 0)
+        {
+            father->left_node = child->right_node;
+            child->right_node = father;
+
+            child->balance = 0;
+
+            father->balance = 0;
+
+            if (father->left_node != NULL)
+                father->balance++;
+
+            if (father->right_node != NULL)
+                father->balance--;
+
+            if (father->balance != 0)
+                child->balance--;
+
+            return child;
         }
         else
         {
-            node->balance++;
+            grand_child = child->right_node;
+            child->right_node = grand_child->left_node;
+            grand_child->left_node = child;
+            father->left_node = grand_child->right_node;
+            grand_child->right_node = father;
 
-            result = insert_tree(node->left_node, value);
+            father->balance = 0;
+            child->balance = 0;
 
-            if (result == 0)
-                node->balance--;
+            if (grand_child->balance == 1)
+                father->balance = -1;
+            else if (grand_child->balance == -1)
+                child->balance = 1;
+
+            grand_child->balance = 0;
+
+            return grand_child;
         }
     }
     else
     {
-        if (node->right_node == NULL)
-        {
-            node->right_node = create_tree(value);
-            node->balance--;
+        child = father->right_node;
 
-            if (node->left_node == NULL)
-                return 1;
-            else
-                return 0;
+        if (child->balance <= 0)
+        {
+            father->right_node = child->left_node;
+            child->left_node = father;
+
+            child->balance = 0;
+
+            father->balance = 0;
+
+            if (father->left_node != NULL)
+                father->balance++;
+
+            if (father->right_node != NULL)
+                father->balance--;
+
+            if (father->balance != 0)
+                child->balance++;
+
+            return child;
         }
         else
         {
-            node->balance--;
+            grand_child = child->left_node;
+            child->left_node = grand_child->right_node;
+            grand_child->right_node = child;
+            father->right_node = grand_child->left_node;
+            grand_child->left_node = father;
 
-            result = insert_tree(node->right_node, value);
+            father->balance = 0;
+            child->balance = 0;
 
-            if (result == 0)
+            if (grand_child->balance == 1)
+                child->balance = -1;
+            else if (grand_child->balance == -1)
+                father->balance = 1;
+
+            grand_child->balance = 0;
+
+            return grand_child;
+        }
+    }
+}
+
+tree *remove_node(tree *node)
+{
+    tree *child, *father;
+
+    int found, old_balance;
+
+    if (node->left_node == NULL && node->right_node == NULL)
+    {
+        free(node);
+        node = NULL;
+    }
+    else if (node->left_node == NULL)
+    {
+        child = node;
+        node = node->right_node;
+        free(child);
+    }
+    else if (node->right_node == NULL)
+    {
+        child = node;
+        node = node->left_node;
+        free(child);
+    }
+    else
+    {
+        father = node;
+        child = node->right_node;
+
+        while (child->left_node != NULL)
+        {
+            father = child;
+            child = child->left_node;
+        }
+
+        node->value = child->value;
+
+        if (father == node)
+        {
+            father->right_node = child->right_node;
+
+            father->balance++;
+        }
+        else
+        {
+            old_balance = node->right_node->balance;
+            node->right_node = remove_tree(node->right_node, node->value, &found);
+
+            if (abs(old_balance) > abs(node->right_node->balance))
                 node->balance++;
+        }
+
+        if (abs(node->balance) == 2)
+            node = balance_tree(node);
+    }
+
+    return node;
+}
+
+tree *remove_tree(tree *node, int value, int *found)
+{
+    int old_balance, left = 0;
+
+    tree *remove_child = NULL, *new_child;
+
+    if (node != NULL)
+    {
+        if (node->value == value)
+        {
+            *found = 1;
+            return remove_node(node);
+        }
+        else if (value < node->value)
+        {
+            remove_child = node->left_node;
+
+            if (remove_child != NULL)
+                old_balance = remove_child->balance;
+
+            left = 1;
+        }
+        else
+        {
+            remove_child = node->right_node;
+
+            if (remove_child != NULL)
+                old_balance = remove_child->balance;
+        }
+
+        new_child = remove_tree(remove_child, value, found);
+
+        if (remove_child != NULL)
+        {
+            if (new_child == NULL)
+                if (left == 1)
+                    node->balance--;
+                else
+                    node->balance++;
+            else if (abs(old_balance) > abs(new_child->balance))
+                if (left == 1)
+                    node->balance--;
+                else
+                    node->balance++;
+
+            if (left == 1)
+                node->left_node = new_child;
+            else
+                node->right_node = new_child;
+
+            if (abs(node->balance) == 2)
+                node = balance_tree(node);
         }
     }
 
-    if (abs(node->balance) >= 2)
-    {
-    }
-
-    return result;
+    return node;
 }
 
-void print_tree(tree *node, int level)
+tree *insert_tree(tree *node, int value)
 {
-    // Printa a arvore binario na forma horizontal
+    tree *new_node, *insert_node;
+
+    int balance = 0, old_balance;
+
+    if (node == NULL)
+        return create_tree(value);
+    else if (value < node->value)
+    {
+        balance = 1;
+        insert_node = node->left_node;
+    }
+    else
+    {
+        balance = -1;
+        insert_node = node->right_node;
+    }
+
+    if (insert_node != NULL)
+        old_balance = insert_node->balance;
+
+    new_node = insert_tree(insert_node, value);
+
+    if (balance > 0)
+        node->left_node = new_node;
+    else
+        node->right_node = new_node;
+
+    if (insert_node != NULL)
+        if (new_node->balance == 0 || new_node->balance == old_balance)
+            balance = 0;
+        else if (abs(old_balance) > abs(new_node->balance))
+            balance = -balance;
+
+    node->balance += balance;
+
+    if (abs(node->balance) == 2)
+        node = balance_tree(node);
+
+    return node;
+}
+
+void free_tree(tree *node)
+{
     if (node != NULL)
     {
-        int i;
-        print_tree(node->left_node, level + 1);
-        for (i = 0; i < level; i++)
-            printf("\t");
-        printf("%d (%d)\n", node->value, node->balance);
-        print_tree(node->right_node, level + 1);
+        free_tree(node->left_node);
+        free_tree(node->right_node);
+        free(node);
     }
+}
+
+int get_height(tree *node)
+{
+    int height = 0;
+
+    if (node != NULL)
+        if (node->balance >= 0)
+            return 1 + get_height(node->left_node);
+        else
+            return 1 + get_height(node->right_node);
+    else
+        return 0;
+}
+
+tree *get_node(tree *node, int value)
+{
+
+    if (node != NULL)
+    {
+        if (node->value == value)
+            return node;
+        else if (value < node->value)
+            return get_node(node->left_node, value);
+        else
+            return get_node(node->right_node, value);
+    }
+
+    return NULL;
 }
 
 int main(int argc, char const *argv[])
 {
-    int n;
+    int value = 0, found;
 
-    scanf("%d", &n);
+    tree *node = NULL;
 
-    tree *node = create_tree(n);
-
-    while (n > -1)
+    while (value > -1)
     {
-        scanf("%d", &n);
+        scanf("%d", &value);
 
-        if (n > -1)
-            insert_tree(node, n);
+        if (value > -1)
+            node = insert_tree(node, value);
     }
 
-    print_tree(node, 0);
+    value = 0;
+
+    printf("%d, %d, %d\n", get_height(node) - 1, get_height(node->left_node), get_height(node->right_node));
+
+    while (value > -1)
+    {
+        scanf("%d", &value);
+
+        if (value > -1)
+        {
+            found = 0;
+
+            node = remove_tree(node, value, &found);
+
+            if (found == 0)
+                node = insert_tree(node, value);
+        }
+    }
+
+    scanf("%d", &value);
+
+    tree *result = get_node(node, value);
+
+    if (result != NULL)
+        printf("%d, %d, %d", get_height(result) - 1, get_height(result->left_node), get_height(result->right_node));
+    else
+        printf("Valor nao encontrado");
+
+    free_tree(node);
 
     return 0;
 }
