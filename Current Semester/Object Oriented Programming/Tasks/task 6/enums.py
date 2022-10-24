a1f9ab2f814c8fc70f8c7c82ecf732cce7d067bf
Victor Jorge Carvalho Chaves <victorchaves03@gmail.com>
2022-10-24 17:46:02 -0300

    feat(poo) : task 6 done

---
+++ Current Semester/Object Oriented Programming/Tasks/task 6/enums.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class Priority(Enum):
+    HIGH = 3
+    MEDIAN = 2
+    LOW = 1
---
+++ Current Semester/Object Oriented Programming/Tasks/task 6/todolist.py
@@ -0,0 +1,36 @@
+from user import User
+from todoitem import TodoItem
+
+
+class TodoList:
+    def __init__(self, owner: User):
+        self.owner = owner
+        self.list = []
+
+    def add(self, item: TodoItem):
+        self.list.append(item)
+
+    def get(self, index):
+        return self.list[index]
+
+    def get_owner(self):
+        return self.owner
+
+    def complete_item(self, index):
+        self.list[index].complete()
+
+    def remove(self, index):
+        self.list.pop(index)
+
+    def size(self):
+        return len(self.list)
+
+    def find(self, description):
+        for item in self.list:
+            if item.description == description:
+                return item
+
+    def list_items(self):
+        self.list.sort(reverse=True, key=lambda item: item.priority.value)
+
+        return self.list
