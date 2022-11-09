9c3f49141b394ace1c21b43a566b2e04fb16c0ae
Victor Jorge Carvalho Chaves <victorchaves03@gmail.com>
2022-11-08 22:51:41 -0300

    update

---
+++ Current Semester/Object Oriented Programming/Exams/Exam 1/classes/Car.py
@@ -0,0 +1,6 @@
+class Car:
+    def __init__(self, brand: str, model: str, year: int, price: float):
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.price = price
