1e871a959d6ec09aa0bee6cad775906992d78070
Victor Jorge Carvalho Chaves <victorchaves03@gmail.com>
2022-12-15 20:49:11 -0300

    update

--- Current Semester/Database/Tasks/task 17/task.sql
+++ Current Semester/Database/Tasks/task 17/task.sql
@@ -10,20 +10,20 @@ INNER JOIN circuito c ON c.codcirc = p2.codcirc;
 #de prova, listar a data da prova, duração e posição do piloto na prova.
 
 SELECT p.nomepil, p3.`data`, p3.duracao, p2.posicao FROM piloto p 
-LEFT OUTER JOIN pospilotoprova p2 ON p2.codpil = p.codpil
+LEFT JOIN pospilotoprova p2 ON p2.codpil = p.codpil
 LEFT JOIN prova p3 ON p3.codcirc = p2.codcirc AND p3.`data` = p2.`data`;
-
 
 #3) Selecionar o nome do piloto, seu país de origem, nome da equipe e país da equipe.
 
-SELECT p.nomepil, p3.nomepais origem, e.nomeeq  ,p2.nomepais pais_eqp FROM piloto p 
+SELECT p.nomepil, p3.nomepais origem, e.nomeeq, p2.nomepais pais_eqp FROM piloto p 
 INNER JOIN equipe e ON e.codeq = p.codeq 
 INNER JOIN pais p2 ON p2.codpais = e.codpais 
-INNER JOIN pais p3 ON p3.codpais = p.codpais 
+INNER JOIN pais p3 ON p3.codpais = p.codpais; 
 
 #4) Listar o nome das equipes que não possuem piloto.
 
-
+SELECT e.nomeeq  FROM equipe e 
+WHERE e.codeq NOT IN (SELECT p.codeq  FROM piloto p)
 
 #5) Listar os nomes dos países que não existem pilotos (usar NOT EXISTS)
 
