05cf5274aebcf803fb0379c7dcc5cedaa12b60f9
Victor Jorge Carvalho Chaves <victorchaves03@gmail.com>
2022-12-14 22:02:57 -0300

    update

---
+++ Current Semester/Database/Tasks/task 18/task.sql
@@ -0,0 +1,19 @@
+#Utilizando a base de dados de Piloto, crie visões para cada uma das consultas abaixo:
+#1) Selecionar o nome dos pilotos e nome dos circuitos que eles participaram
+
+#2) Selecionar o nome de todos os pilotos do banco de dados e para aqueles que participaram de prova, listar a data da prova, duração e posição do piloto na prova.
+
+#3) Selecionar o nome do piloto, seu país de origem, nome da equipe e país da equipe.
+
+#4) Listar o nome das equipes que não possuem piloto
+
+#Usando a visões criadas, selecione:
+#5) Para cada nome de piloto, indique quantos circuitos eles participaram
+
+#6) Listar o nome do piloto e o nome da sua equipe
+
+#7) Listar os pilotos que são do mesmo país que a sua equipe
+
+#8) Inserir um novo piloto no banco de dados
+
+#9) Execute um select * na view criada na questão 2) e diga o que ocorreu.
