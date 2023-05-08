64f6d019d723a80d9b69c1e7fcd27fc35f69279f
Victor Jorge Carvalho Chaves <victorchaves03@gmail.com>
2023-05-07 23:18:31 -0300

    update

--- Past Semesters/Third Semester/Algorithms and Data Structure II/Tasks/task 7/main.c
+++ Past Semesters/Third Semester/Algorithms and Data Structure II/Tasks/task 7/main.c
@@ -4,38 +4,56 @@
 
 typedef char *string;
 
-int get_index(char c)
+int get_index(char c, char *keys)
 {
     if (c <= 32)
         return 0;
     else
-        return c - 96;
+    {
+        int i = 0;
+
+        for (i = 1; i < 27; i++)
+        {
+            // printf("t : %c == %c\n", c, keys[i - 1]);
+            if (keys[i - 1] == c)
+                return i;
+        }
+    }
 }
 
-string *counting_sort(string *a, string *b, int n, int k, int d)
+string *counting_sort(string *input, string *output, int length, int keys, int column, char *chars)
 {
-    int *c = (int *)malloc(sizeof(int) * k);
+    int *vector = (int *)malloc(sizeof(int) * keys);
 
-    int i;
+    int index;
 
-    for (i = 0; i < k; i++)
-        c[i] = 0;
+    for (index = 0; index < keys; index++)
+        vector[index] = 0;
 
-    for (i = 0; i < n; i++)
-        c[get_index(a[i][d])]++;
+    int letter_index = 0;
 
-    for (i = 0; i < k; i++)
+    for (index = 0; index < length; index++)
     {
-        c[i] += c[i - 1];
-        printf("%d ", c[i]);
+        letter_index = get_index(input[index][column], chars);
+        vector[letter_index]++;
+    }
+
+    for (index = 0; index < keys; index++)
+    {
+        vector[index] += vector[index - 1];
+        printf("%d ", vector[index]);
     }
 
     printf("\n");
 
-    for (i = n - 1; i >= 0; i--)
-        b[--c[get_index(a[i][d])]] = a[i];
+    for (index = length - 1; index >= 0; index--)
+    {
+        letter_index = get_index(input[index][column], chars);
 
-    return b;
+        output[--vector[letter_index]] = input[index];
+    }
+
+    return output;
 }
 
 int main(int argc, char const *argv[])
@@ -45,7 +63,10 @@ int main(int argc, char const *argv[])
 
     int start, number_words, length, i, j, word_length = 0, max_word = 0;
 
+    char keys[27];
+
     scanf("%d", &length);
+    scanf("%s", keys);
 
     strings = (string *)malloc(sizeof(string) * length);
 
@@ -55,9 +76,6 @@ int main(int argc, char const *argv[])
 
         scanf("%s", strings[i]);
     }
-
-    scanf("%d", &start);
-    scanf("%d", &number_words);
 
     for (i = 0; i < length; i++)
     {
@@ -81,10 +99,10 @@ int main(int argc, char const *argv[])
     for (i = max_word - 1; i >= 0; i--)
     {
         output = (string *)malloc(sizeof(string) * length);
-        strings = counting_sort(strings, output, length, 27, i);
+        strings = counting_sort(strings, output, length, 27, i, keys);
     }
 
-    for (i = start - 1; i < start + number_words - 1; i++)
+    for (i = 0; i < length; i++)
     {
         printf("%s\n", strings[i]);
         free(strings[i]);
