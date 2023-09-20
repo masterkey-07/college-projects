c81df2c6180883251a2f2316b680fcec99108cee
Victor Jorge Carvalho Chaves <victorchaves03@gmail.com>
2023-09-19 21:59:08 -0300

    update

--- Current Semester/Computer Architecture and Organization/Tasks/task 1/main2.asm
+++ Current Semester/Computer Architecture and Organization/Tasks/task 1/main2.asm
@@ -1,39 +1,32 @@
-# Simple input/output in MIIPS assembly
-# From: http://labs.cs.upt.ro/labs/so2/html/resources/nachos-doc/mipsf.html
+# victor jorge carvalho chaves
 
-# Start .text segment (program code)
 .text
 
 .globl	main
 main:
-                                            # Get input A from user and save
-    li      $v0,    5                       # read_int syscall code = 5
+    li      $v0,    5
     syscall 
-    move    $t0,    $v0                     # syscall results returned in $v0
+    move    $t0,    $v0
 
-
-# Get input B from user and save
-    li      $v0,    5                       # read_int syscall code = 5
+    li      $v0,    5
     syscall 
-    move    $t1,    $v0                     # syscall results returned in $v0
+    move    $t1,    $v0
 
-
-# Get input C from user and save
-    li      $v0,    5                       # read_int syscall code = 5
+    li      $v0,    5
     syscall 
-    move    $t2,    $v0                     # syscall results returned in $v0
+    move    $t2,    $v0
 
 compab:
-    bge     $t0,    $t1,        compac
-    bge     $t1,    $t0,        compbc
+    bge     $t0,    $t1,    compac
+    bge     $t1,    $t0,    compbc
 
 compac:
-    bge     $t0,    $t2,        biggera
-    bge     $t2,    $t0,        biggerc
+    bge     $t0,    $t2,    biggera
+    bge     $t2,    $t0,    biggerc
 
 compbc:
-    bge     $t1,    $t2,        biggerb
-    bge     $t2,    $t1,        biggerc
+    bge     $t1,    $t2,    biggerb
+    bge     $t2,    $t1,    biggerc
 
 biggera:
     move    $t3,    $t0
@@ -48,20 +41,17 @@ biggerc:
     j       final
 
 final:
-                                            # Print sum
-    li      $v0,    1                       # print_int syscall code = 1
-    move    $a0,    $t3                     # int to print must be loaded into $a0
+    li      $v0,    4
+    la      $a0,    maior
     syscall 
 
 
-# Print \n
-    li      $v0,    4                       # print_string syscall code = 4
-    la      $a0,    newline
+    li      $v0,    1
+    move    $a0,    $t3
     syscall 
 
-    li      $v0,    10                      # exit
+    li      $v0,    10
     syscall 
 
-# Start .data segment (data!)
 .data
-newline:   .asciiz	"\n"
+maior: .asciiz "Maior: "
