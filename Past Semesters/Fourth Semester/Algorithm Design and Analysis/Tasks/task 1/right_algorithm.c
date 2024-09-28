2dda865258de7bd73e0a286c6ff8e1847ed1bfba
Victor Jorge Carvalho Chaves <victorchaves03@gmail.com>
2024-09-28 11:04:46 -0300

    feito
    Co-authored-by: pedri10pop <pedri10pop@users.noreply.github.com>

--- Past Semesters/Fourth Semester/Algorithm Design and Analysis/Tasks/task 1/right_algorithm.c
+++ Past Semesters/Fourth Semester/Algorithm Design and Analysis/Tasks/task 1/right_algorithm.c
@@ -1,19 +1,33 @@
+#include <math.h>
 #include <stdio.h>
-#define line 1026
-#define colum 1026
+#include <stdlib.h>
+
+#define MAX_WIDTH 1026
+#define MAX_HEIGHT 1026
+
+int MIN(int a, int b)
+{
+    return a > b ? b : a;
+}
+
+int MAX(int a, int b)
+{
+    return a > b ? a : b;
+}
 
 int main(int argc, char const *argv[])
 {
-    int agglomeration[line][colum]; // line - position and how many users; colum - position(x, y) and users
-    int square_size;                // square's size; 1<=d<=50
-    int number_positions;           // numbers of position; 1<=n<=25000
-    int x, y;                       // quarter's position (x, y)
-    int limit_x, limit_y;           // limit of square; ydt -> y top; ydi -> y bottom
-    int quantity_users;             // quantity of users
-    int i;                          // for's variable
-    int j;                          // for's variable
-    int k;                          // for's variable
-    int max = 0;                    // max value
+    int agglomeration[MAX_WIDTH][MAX_HEIGHT]; // line - position and how many users; column - position(x, y) and users
+    int square_size;                          // square's size; 1<=d<=50
+    int number_positions;                     // numbers of position; 1<=n<=25000
+    int x, y;                                 // quarter's position (x, y)
+    int max_limit_x, max_limit_y;             // limit of square; ydt -> y top; ydi -> y bottom
+    int min_limit_x, min_limit_y;             // limit of square; ydt -> y top; ydi -> y bottom
+    int quantity_users;                       // quantity of users
+    int i;                                    // for's variable
+    int j;                                    // for's variable
+    int k;                                    // for's variable
+    int max_agglomeration = 0;                // max value
 
     for (j = 0; j < 1026; j++)
         for (k = 0; k < 1026; k++)
@@ -26,31 +40,33 @@ int main(int argc, char const *argv[])
     {
         scanf("%i %i %i", &x, &y, &quantity_users);
 
-        limit_x = x + square_size + 1;
+        max_limit_x = MIN(x + square_size + 1, 1025);
+        min_limit_x = MAX(x - square_size, 0);
 
-        for (j = x - square_size; j < limit_x; j++)
-            if (j > -1 && j < 1026)
+        for (j = min_limit_x; j < max_limit_x; j++) // areas possíve que a grarita cobrirá esse conjunto de pessoas
+        {
+            max_limit_y = MIN(y + square_size + 1, 1025);
+            min_limit_y = MAX(y - square_size, 0);
+
+            for (k = min_limit_y; k < max_limit_y; k++)
             {
-                limit_y = y + square_size + 1;
+                if (agglomeration[j][k] > 0)
+                    agglomeration[j][k] = agglomeration[j][k] + quantity_users;
+                else
+                    agglomeration[j][k] = quantity_users;
 
-                for (k = y - square_size; k < limit_y; k++)
-                    if (k > -1 && k < 1026)
-                    {
-                        if (agglomeration[j][k] > 0)
-                            agglomeration[j][k] = agglomeration[j][k] + quantity_users;
-                        else
-                            agglomeration[j][k] = quantity_users;
-
-                        if (agglomeration[j][k] > max)
-                            max = agglomeration[j][k];
-                    }
+                if (agglomeration[j][k] > max_agglomeration)
+                {
+                    max_agglomeration = agglomeration[j][k];
+                }
             }
+        }
     }
 
     j = 1;
     k = 1;
 
-    while (agglomeration[j][k] != max)
+    while (agglomeration[j][k] != max_agglomeration)
     {
         k++;
         if (k == 1026)
@@ -60,7 +76,7 @@ int main(int argc, char const *argv[])
         }
     }
 
-    printf("%i %i %i", j, k, max);
+    printf("%i %i %i", j, k, max_agglomeration);
 
     return 0;
 }
